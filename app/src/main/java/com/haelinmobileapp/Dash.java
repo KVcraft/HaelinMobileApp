768e47b7c3391cdae3a811ffa0067634bb352c22
M H K Viduranga <kavinduv69@gmail.com>
2025-11-18 10:30:05 +0530

    Ignored gradle files

--- app/src/main/java/com/haelinmobileapp/Dash.java
+++ app/src/main/java/com/haelinmobileapp/Dash.java
@@ -1,25 +1,58 @@
 package com.haelinmobileapp;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.haelinmobileapp.databinding.ActivityDashBinding;
+import com.haelinmobileapp.R;
 
 public class Dash extends AppCompatActivity {
+
+    @NonNull ActivityDashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        binding = ActivityDashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Dashboard());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(new Dashboard());
+            } else if (itemId == R.id.chatBot) {
+                replaceFragment(new Chatbot());
+            } else if (itemId == R.id.medHistory) {
+                replaceFragment(new MedHistory());
+            }
+
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 
 
